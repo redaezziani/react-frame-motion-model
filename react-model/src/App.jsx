209933e4df0957c96e1c411d903ecea4bf3cb756
redaezziani <2003100500167@ofppt-edu.ma>
2023-09-21 22:40:00 +0200

    add dark mode

--- react-model/src/App.jsx
+++ react-model/src/App.jsx
@@ -1,27 +1,35 @@
-import { AnimatePresence } from 'framer-motion'
-import './App.css'
-import Model from './components/Model'
-import { useState } from 'react'
+import { AnimatePresence } from 'framer-motion';
+import './App.css';
+import Model from './components/Model';
+import { useModel, ModelProvider } from './Contexts/ModelContext'; // Import the useModel hook
+import React, { useState } from 'react';
+
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
-  const title='Simple Model'
-  const handelOpen=()=>{
-    setIsOpen(!isOpen)
-  }
-  const type='blur'
-  const content='This is a simple model component made with react and framer-motion library to show how to use react with framer-motion.'
+  const { isOpen, toggleModel } = useModel(); // Use the useModel hook to access isOpen and toggleModel
+  const title = 'Simple Model';
+  const type = 'blur';
+  const content = 'This is a simple model component made with react and framer-motion library to show how to use react with framer-motion.';
+
   return (
-    <div
-    className='flex items-center justify-center min-h-screen bg-gray-200 relative'
-    >
-      <button  onClick={handelOpen} className='rounded-md text-white dark:text-slate-100 min-w-[5rem] bg-slate-800 px-3 py-[0.38rem]'>
+    <div className='flex items-center justify-center min-h-screen bg-gray-200 relative'>
+      <button onClick={toggleModel} className='rounded-md text-white dark:text-slate-100 min-w-[5rem] bg-slate-800 px-3 py-[0.38rem]'>
         Open
       </button>
       <AnimatePresence>
-      <Model title={title} Open={isOpen} type={type} content={content} />
+        {isOpen && <Model title={title} Open={isOpen} type={type} content={content} />}
       </AnimatePresence>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default function AppWrapper() {
+  return (
+    <ModelProvider>
+      <App />
+    </ModelProvider>
+  );
+}
+
+// Path: react-model/src/components/Model.jsx
+
---
+++ react-model/src/Contexts/ModelContext.jsx
@@ -0,0 +1,27 @@
+// ModelContext.js
+import React, { createContext, useContext, useState } from 'react';
+
+// Create the context
+const ModelContext = createContext();
+
+export const ModelProvider = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModel = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <ModelContext.Provider value={{ isOpen, toggleModel }}>
+      {children}
+    </ModelContext.Provider>
+  );
+};
+
+export const useModel = () => {
+  const context = useContext(ModelContext);
+  if (!context) {
+    throw new Error('useModel must be used within a ModelProvider');
+  }
+  return context;
+};
