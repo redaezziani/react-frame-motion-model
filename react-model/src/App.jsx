5a9debf87f4371b3d8efd999a5c3195792f4bd3f
redaezziani <2003100500167@ofppt-edu.ma>
2023-09-22 22:09:16 +0200

    last test for the model

--- react-model/src/App.jsx
+++ react-model/src/App.jsx
@@ -1,27 +1,33 @@
 import { AnimatePresence } from "framer-motion";
 import "./App.css";
-//import Model from "./components/Model";
+import Model from "./components/Model";
 //import Notification from "./components/Notification";
-import UploadInput from "./components/Upload";
 import { useModel, ModelProvider } from "./Contexts/ModelContext";
 
 
 function App() {
   const { isOpen, toggleModel } = useModel(); 
-  //const title = "Simple Model"; 
-  //const type = "blur";//simple note if you dont want to use blur effect then use "none" to set mode default
+  const title = "Simple Model"; 
+  const type = "blur";//simple note if you dont want to use blur effect then use "none" to set mode default
   //const NotifacationType="";
-  //const content ="this is a simple model component that you can use in your project like a notification or a simple model.";
+  const content ="this is a simple model component that you can use in your project like a notification or a simple model.";
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 relative">
-      {/*<button
+    <div className="flex items-center justify-center min-h-screen bg-gray-300 relative">
+      <button
         onClick={toggleModel}
         className="rounded-md text-white dark:text-slate-100 min-w-[5rem] bg-slate-800 px-3 py-[0.38rem]"
       >
         Open
-      </button>*/}
+      </button>
       <AnimatePresence>
-        <UploadInput />
+        {isOpen && (
+          <Model
+            title={title}
+            content={content}
+            type={type}
+            toggleModel={toggleModel}
+          />
+        )}
       </AnimatePresence>
     </div>
   );
